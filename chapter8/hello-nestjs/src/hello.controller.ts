cff85ce9228584be9defcde9866ef45117ca7122
gaebaljoah <ljm5436124@naver.com>
2023-10-24 16:29:33 +0900

    chapter8 삭제

--- chapter8/hello-nestjs/src/hello.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from "@nestjs/common"; //1.필요한 함수 임포트
-
-@Controller()   //2.컨트롤러 데코레이터
-export class HelloController{   //3. 외부에서 사용하므로 export를 붙혀줍니다.
-    @Get()      //4. GET 요청 처리 데코레이터
-    hello(){
-        return "안녕하세요! NestJS로 만든 첫 어플리케이션입니다,";
-    }
-}
